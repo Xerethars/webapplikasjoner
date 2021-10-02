64e71820c3665f519ffd0f1a97090336c633cae1
Michal Kowalski <michalk@hiof.no>
2021-10-02 19:02:18 +0200

    react set up and working, navbar implemented

---
+++ oppgave6/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavBar from './components/NavBar';
+
+const App = () => {
+  return (
+    <body>
+      <NavBar userName='User user'/>
+    </body>
+  );
+}
+
+export default App;
---
+++ oppgave6/src/components/NavBar.js
@@ -0,0 +1,8 @@
+const NavBar = ({userName}) => (
+    <header>
+        <h2>HIOF</h2>
+        <p>{userName}</p>
+    </header>
+);
+
+export default NavBar;
